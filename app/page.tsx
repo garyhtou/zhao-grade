2967f7d983f1e54db2362fc95961cba19dd20527
Gary Tou <gary@garytou.com>
2023-05-31 15:23:00 -0700

    Refactoring grade

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,258 @@
-import Image from 'next/image'
+'use client';
+
+import { useState } from 'react';
+import { Button, Form, InputNumber } from 'antd';
+import { ConfigProvider, theme } from 'antd';
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+	const [grade, setGrade] = useState<number | null>(null);
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+	const onFinish = (values: any) => {
+		console.log('Success:', values);
+		const {
+			ic1,
+			ic2,
+			ic3,
+			ic4,
+			ic5,
+			ic6,
+			ic7,
+			ic8,
+			ic9,
+			ic10,
+			hw1,
+			hw2,
+			hw3,
+			me,
+			fe,
+			p2mt,
+			pm,
+			pmc,
+			extra,
+		} = values;
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+		const inClass =
+			(ic1 + ic2 + ic3 + ic4 + ic5 + ic6 + ic7 + ic8 + ic9 + ic10) * 0.01;
+		const hw = (hw1 + hw2 + hw3) * 0.05;
+		const project = (p2mt + pm + pmc) * 0.05;
+		const midterm = me * 0.2;
+		const final = (fe + extra) * 0.35;
+		const total = inClass + hw + project + midterm + final;
+		console.log({ inClass, hw, project, midterm, final, total });
+		setGrade(total);
+	};
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+	const onFinishFailed = (errorInfo: any) => {
+		console.log('Failed:', errorInfo);
+	};
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+	return (
+		<ConfigProvider
+			theme={{
+				algorithm: theme.darkAlgorithm,
+			}}
+		>
+			<main className='flex min-h-screen flex-col items-center justify-between p-24'>
+				<div className='z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex'>
+					<p className='fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30'>
+						Calculate your Refactoring grade!
+					</p>
+				</div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+				<div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
+					<Form
+						name='basic'
+						initialValues={{ remember: true }}
+						onFinish={onFinish}
+						onFinishFailed={onFinishFailed}
+						autoComplete='off'
+					>
+						<Form.Item
+							label='Metrics Workshop'
+							name='ic1'
+							rules={[{ required: true }]}
+							initialValue={100}
+						>
+							<InputNumber min={0} max={100} />
+						</Form.Item>
+
+						<Form.Item
+							label='Quality Workshop'
+							name='ic2'
+							rules={[{ required: true }]}
+							initialValue={100}
+						>
+							<InputNumber min={0} max={100} />
+						</Form.Item>
+
+						<Form.Item
+							label='Code Smell Workshop'
+							name='ic3'
+							rules={[{ required: true }]}
+							initialValue={100}
+						>
+							<InputNumber min={0} max={100} />
+						</Form.Item>
+
+						<Form.Item
+							label='High Cohesion Low Coupling In-Class Exercise'
+							name='ic4'
+							rules={[{ required: true }]}
+							initialValue={100}
+						>
+							<InputNumber min={0} max={100} />
+						</Form.Item>
+
+						<Form.Item
+							label='Paper Analysis Inclass Exercise'
+							name='ic5'
+							rules={[{ required: true }]}
+							initialValue={100}
+						>
+							<InputNumber min={0} max={100} />
+						</Form.Item>
+
+						<Form.Item
+							label='Refactoring Workshop'
+							name='ic6'
+							rules={[{ required: true }]}
+							initialValue={100}
+						>
+							<InputNumber min={0} max={100} />
+						</Form.Item>
+
+						<Form.Item
+							label='Research Survey'
+							name='ic7'
+							rules={[{ required: true }]}
+							initialValue={100}
+						>
+							<InputNumber min={0} max={100} />
+						</Form.Item>
+
+						<Form.Item
+							label='Code Smell In-class Exercise'
+							name='ic8'
+							rules={[{ required: true }]}
+							initialValue={100}
+						>
+							<InputNumber min={0} max={100} />
+						</Form.Item>
+
+						<Form.Item
+							label='Design Pattern Inclass Exercise'
+							name='ic9'
+							rules={[{ required: true }]}
+							initialValue={100}
+						>
+							<InputNumber min={0} max={100} />
+						</Form.Item>
+
+						<Form.Item
+							label='Pre-Course Survey'
+							name='ic10'
+							rules={[{ required: true }]}
+							initialValue={100}
+						>
+							<InputNumber min={0} max={100} />
+						</Form.Item>
+
+						<Form.Item
+							label='Homework 1'
+							name='hw1'
+							rules={[{ required: true }]}
+							initialValue={100}
+						>
+							<InputNumber min={0} max={100} />
+						</Form.Item>
+
+						<Form.Item
+							label='Homework 2'
+							name='hw2'
+							rules={[{ required: true }]}
+							initialValue={200}
+						>
+							<InputNumber min={0} max={200} defaultValue={200} />
+						</Form.Item>
+
+						<Form.Item
+							label='Homework 3'
+							name='hw3'
+							rules={[{ required: true }]}
+							initialValue={100}
+						>
+							<InputNumber min={0} max={100} />
+						</Form.Item>
+
+						<Form.Item
+							label='2 Minutes Talk'
+							name='p2mt'
+							rules={[{ required: true }]}
+							initialValue={100}
+						>
+							<InputNumber min={0} max={100} />
+						</Form.Item>
+
+						<Form.Item
+							label='Midpoint Check'
+							name='pmc'
+							rules={[{ required: true }]}
+							initialValue={100}
+						>
+							<InputNumber min={0} max={100} />
+						</Form.Item>
+
+						<Form.Item
+							label='Presentation'
+							name='pm'
+							rules={[{ required: true }]}
+							initialValue={100}
+						>
+							<InputNumber min={0} max={100} />
+						</Form.Item>
+
+						<Form.Item
+							label='Midterm Exam'
+							name='me'
+							rules={[{ required: true }]}
+							initialValue={100}
+						>
+							<InputNumber min={0} max={100} />
+						</Form.Item>
+
+						<Form.Item
+							label='Final Exam'
+							name='fe'
+							rules={[{ required: true }]}
+							initialValue={100}
+						>
+							<InputNumber min={0} max={100} />
+						</Form.Item>
+
+						<Form.Item
+							label='Explanation of Refactoring Technique from Research Papers'
+							name='extra'
+							rules={[{ required: true }]}
+							initialValue={0}
+						>
+							<InputNumber min={0} max={100} />
+						</Form.Item>
+
+						<Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+							<Button type='primary' htmlType='submit'>
+								Submit
+							</Button>
+						</Form.Item>
+					</Form>
+				</div>
+				<div>
+					{grade ? (
+						<p className='text-2xl font-bold text-center'>
+							Your Refactoring grade is: {Math.round(grade * 100) / 100}%
+						</p>
+					) : null}
+				</div>
+			</main>
+		</ConfigProvider>
+	);
 }
